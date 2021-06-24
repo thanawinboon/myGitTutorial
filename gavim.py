271280e9b5d9c79be8a407af7007ffb70a7691ea
Pat Thanawin <pokepat27@gmail.com>
2021-06-25 00:13:06 +0700

    Made more comments

--- gavim.py
+++ gavim.py
@@ -1,3 +1,10 @@
 
 def hello():
     print("Gawim is happy")
+
+# git init  --> initialize your local git
+# git status --> check changes
+# git add  --> add all specified changes that you want to save
+# git commit -m "your commit messages"  --> to confirm your changes and save em
+# git log  --> view commit history
+
